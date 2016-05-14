5e0cd69db10bc36fdf7ed18e801caa7be30ef54a
Hexeption <minecraftfun201@gmail.com>
2016-05-15 00:44:16 +0100

    :bug: Fixed Shaders

--- Patch/Patch Update.bat
+++ Patch/Patch Update.bat
@@ -1,22 +1,12 @@
 @echo off
 
-rem This Batch file is for updating Minecraft patch File
 
-rem branches:
-rem Master - Vinilla MC
-rem Opiates - Modified Opiates MC
-rem Temp - temp branch
 
 set mc-dir="C:\Users\Hexeption\Desktop\Hacked Clients\Workspace\Opiates Minecraft 1.9.X"
 set opiates-dir="C:\Users\Hexeption\Desktop\Hacked Clients\Opiates"
 
 cd %mc-dir%
-git checkout master
-git checkout -b temp
-git merge --squash Opiates
-git commit -a -m "Opiates"
-git format-patch master --ignore-space-change
-git checkout Opiates
-git branch -D temp
-move /y 0001-Opiates.patch %opiates-dir%\Patch\Minecraft.patch
-rem pause
+
+git diff master > %opiates-dir%/Patch/Minecraft.patch
+
+pause
